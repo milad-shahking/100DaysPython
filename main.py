3430d852a509e7b0f1d8a02a8b0beda4dd3f650a
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-02-04 13:22:19 -0600

    day number 02

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello milad')
+total = float(input('what was the total bill?'))
+tip = int(input('How much tip would you like to give?'))
+split = int(input('How many people to split the bill?'))
+
+tipPerson = (tip / 100) * total
+tipPersonTotal = total + tipPerson
+totalPay = tipPersonTotal / split
+print(f'Each person should: {round(totalPay,1)}')
+
