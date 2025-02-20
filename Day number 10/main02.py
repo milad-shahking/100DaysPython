39350e2759520161d63d159845f0792fb02c4021
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-02-20 13:37:18 -0600

    learn some methods

--- Day number 10/main02.py
+++ Day number 10/main02.py
@@ -2,10 +2,10 @@ site = 'nabegheha.com'
 age = 14
 year = 2021
 char = 'm'
-modele = 19.4
+modele = 19.4654654
 
 print('this is my site : %s' %site)
 print('this is mu character : %c' %char)
 print('this is my age : %d' %age)
 print('this is my year : %i' %year)
-print('this is my modele : %f' %modele)
+print('this is my modele : %.2f' %modele)
---
+++ Day number 10/main03.py
@@ -0,0 +1,22 @@
+# format string
+
+name = 'milad'
+age = 35
+char = '-'
+senentens = 'this is a good cours'
+characters = ('a', 'b', 'c')
+
+print(f'my name is {name} and im {age} years old')
+
+print(f'{name:*^50s}')
+
+print(name.capitalize())
+print(name.count('a'))
+
+print(char.join(characters))
+
+print(len(name))
+
+print(senentens.split(' '))
+
+print(senentens.replace('good' , 'great'))
