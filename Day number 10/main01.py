d78204a0c1ccac473337a4239236270e1c0b15a8
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-02-20 12:01:26 -0600

    percent string

---
+++ Day number 10/main01.py
@@ -0,0 +1,9 @@
+name = 'milad '
+family = 'shahkarami'
+
+print(name + family)
+print(name * 2)
+print('m' in name)
+print('g' in family)
+print('g' not in name)
+
---
+++ Day number 10/main02.py
@@ -0,0 +1,11 @@
+site = 'nabegheha.com'
+age = 14
+year = 2021
+char = 'm'
+modele = 19.4
+
+print('this is my site : %s' %site)
+print('this is mu character : %c' %char)
+print('this is my age : %d' %age)
+print('this is my year : %i' %year)
+print('this is my modele : %f' %modele)
