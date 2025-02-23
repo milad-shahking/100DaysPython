240d5af08e22a0752967c93ee29b45d24382bc7c
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-02-23 12:42:37 -0600

    check discount for the pay

--- Day number 12/main02.py
+++ Day number 12/main02.py
@@ -1,8 +1,11 @@
-price = 500
+price = 1500
 
 if price < 500:
     discount = price * 0.05
     print(f'your price is : {discount}')
+elif price > 1200:
+    discount = price * 0.35
+    print(f'your price is : {discount}')
 else:
     discount = price * 0.10
-    print(f'your price is : {discount}')
+    print(f'your price is : {discount}') 
