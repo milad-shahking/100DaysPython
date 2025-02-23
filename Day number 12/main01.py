ee8758b04ef696a27215247cccee7d2cb08d3302
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-02-23 12:20:42 -0600

    if and else

---
+++ Day number 12/main01.py
@@ -0,0 +1,7 @@
+
+num = 15
+
+if num > 10:
+    print('number is bigger than 10')
+else:
+    print('number is not bigger than 10')
---
+++ Day number 12/main02.py
@@ -0,0 +1,8 @@
+price = 500
+
+if price < 500:
+    discount = price * 0.05
+    print(f'your price is : {discount}')
+else:
+    discount = price * 0.10
+    print(f'your price is : {discount}')
