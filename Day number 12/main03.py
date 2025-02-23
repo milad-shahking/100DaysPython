6dfe4d2cff56e77f9ba0ba00d75b7b4d8f635ae3
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-02-23 12:47:16 -0600

    check pos or neg for enternce number

---
+++ Day number 12/main03.py
@@ -0,0 +1,9 @@
+num = float(input('Enter ypur number : '))
+
+if num >= 0:
+    if num == 0:
+        print('your number is zero')
+    else:
+        print('your number is positive')
+else:
+    print('your number is negetive')
