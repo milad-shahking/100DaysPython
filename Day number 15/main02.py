9bcc0153681f5d91b57dd946c28076096ce1886a
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-03-02 12:26:56 -0600

    key variable

---
+++ Day number 15/main02.py
@@ -0,0 +1,10 @@
+def SayHello(name = 'Your Name', greet = 'Your say lang'):
+    return f'{greet} {name}'
+
+print(SayHello('milad', 'Hi'))
+
+print(SayHello('poo', 'Hey'))
+
+
+print(SayHello(name= 'lili', greet= 'salam'))
+
