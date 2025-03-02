442d5cdcb9f0f762454a7acf7b3b8d8ec3b7ba04
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-03-02 13:38:24 -0600

    lambda

---
+++ Day number 15/main05.py
@@ -0,0 +1,20 @@
+
+
+moraba_kamel = lambda num : num**2
+
+print(moraba_kamel(6))
+
+
+even = lambda inti : inti % 2 == 0
+
+print(even(5))
+
+first_char = lambda FS:FS[0]
+
+print(first_char('milad'))
+
+sum_two_number = lambda x,y : x + y
+
+print(sum_two_number(5 , 8))
+
+# i need to learn python part2 => section06 => 01string =>01
