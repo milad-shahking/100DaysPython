e3969e84e354f97f2291c573c602e5f9e6f022c9
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-03-04 14:05:31 -0600

    some example from all thongs that we learned

---
+++ Day number 16/main.py
@@ -0,0 +1,19 @@
+#name = input('Enter your name :')
+#family = input('Enter your last name :')
+#print(f'{family} {name}')
+#--------------------------------------------
+#word = input('Enter the word : ')
+#print(word.lower())
+#print(word.upper())
+#--------------------------------------------
+number1 = int(input('Enter number 01 : '))
+number2 = int(input('Enter number 02 : '))
+number3 = int(input('Enter number 03 : '))
+listnumber = []
+listnumber.append(number1)
+listnumber.append(number2)
+listnumber.append(number3)
+print(sorted(listnumber))
+#--------------------------------------------
+
+# i need to learn python part 02 section 06 => 01 string => 03 example
