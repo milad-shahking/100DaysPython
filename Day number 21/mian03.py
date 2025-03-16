ab78f65b4696b3a0b7478fa9ded497cc867e6279
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-03-16 23:05:03 +0330

    learn about datetype of boolean

---
+++ Day number 21/mian03.py
@@ -0,0 +1,16 @@
+number_one = 45
+number_two = 12
+type_number = True
+list_num = [1,2,3,4,5,6]
+
+print(number_one < number_two)
+print(number_one > number_two)
+print(number_one == number_two)
+print(number_one <= number_two)
+print(number_one >= number_two)
+print(type(number_one))
+print(type(number_one > number_two))
+
+print(number_one < 40 or number_two > 13)
+print(number_one > 50 and number_two == 12)
+print(2 in list_num)
