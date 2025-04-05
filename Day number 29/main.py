932c382c9c405c8ea6d4febaca9a818228684399
milad shahkarami <151388297+milad-shahking@users.noreply.github.com>
2025-04-05 22:23:28 +0330

    OOP in python

---
+++ Day number 29/main.py
@@ -0,0 +1,9 @@
+class ClassName():
+    def __init__(self, param1):
+        self.param1 = param1
+        print('object created')
+    def say_hello(self):
+        print('hello')
+
+t = ClassName(5)
+print(t.param1)
